4e2588fcf31d24ae4a1b118a003f878e6b934001
Amir hosein <a.h.mohammadi5@gmail.com>
2024-07-02 12:10:05 +0330

    install app-module-path and use it for learn

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+// ? for learn - just install & use for require app in this file
+require('app-module-path').addPath(__dirname);
+// ? for learn - just install & use for layout
 require('dotenv').config();
-
-const app = require('./app');
-
 // console.log(process.env.PUBLIC_DIR);
+
+const app = require('app');
 
 new app();
